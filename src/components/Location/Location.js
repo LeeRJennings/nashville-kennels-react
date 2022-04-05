78c5e5138774257a2b8a7feb855732488c49f45f
LeeRJennings <leerjennings@outlook.com>
2022-04-05 14:38:58 -0500

    initial commit

---
+++ src/components/Location/Location.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Location.css"
+
+export const LocationCard = () => (
+    <section className="location">
+        <h3 className="location__name">Nashville Kennels West</h3>
+        <div className="location__address">24 Harding Ave</div>
+    </section>
+)
